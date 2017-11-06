93756877e3f301c2a17f78a01789228eaa551b4c
Andy TTS <awhillans@techtalentsouth.com>
2017-11-06 09:36:12 -0500

    Added a few routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'practice/index'
+
+  get 'practice/about'
+
+  get 'about' => 'practice#about'
+  get 'index' => 'practice#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
