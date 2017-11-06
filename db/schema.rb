0e50cae9784de69fb525913251301522e29ce650
Andy TTS <awhillans@techtalentsouth.com>
2017-11-06 12:02:12 -0500

    Create one to many relationship between comments and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106161256) do
+ActiveRecord::Schema.define(version: 20171106162538) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author"
     t.text "comment_entry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
