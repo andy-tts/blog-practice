e57b8c0d7880154e3628f19f13389bdb6709c561
Andy TTS <awhillans@techtalentsouth.com>
2017-11-07 10:00:49 -0500

    Display current user's email in posts and comments, and accept user_id in the posts and comments controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -71,6 +71,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body, :author)
+      params.require(:post).permit(:title, :body, :user_id)
     end
 end
