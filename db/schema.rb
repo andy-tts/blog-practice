4f3de9a2b52857ad520ab7901f17f4087f39c1cf
Andy TTS <awhillans@techtalentsouth.com>
2017-11-08 09:09:57 -0500

    Try using different encryption method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107154009) do
+ActiveRecord::Schema.define(version: 20171108140223) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment_entry"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20171107154009) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "password_salt"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
